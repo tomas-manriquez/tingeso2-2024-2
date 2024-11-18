94edddc4a52a4ecf89dec11e11730b7587e30182
tomas-manriquez <tomas.manriquez@usach.cl>
2024-11-18 04:38:18 -0300

    actual-registro-usuario 1

---
+++ actual-registro-usuario/src/main/java/com/tingeso/actual_registro_usuario/ActualRegistroUsuarioApplication.java
@@ -0,0 +1,13 @@
+package com.tingeso.actual_registro_usuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActualRegistroUsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActualRegistroUsuarioApplication.class, args);
+	}
+
+}
---
+++ actual-registro-usuario/src/test/java/com/tingeso/actual_registro_usuario/ActualRegistroUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tingeso.actual_registro_usuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActualRegistroUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
