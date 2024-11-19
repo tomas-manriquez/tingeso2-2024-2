45b1f797d667c437161ac915a738e9bd314927e9
tomas-manriquez <tomas.manriquez@usach.cl>
2024-11-19 06:45:27 -0300

    m3-solicitud-credito 5

--- m3-solicitud-credito/src/main/java/com/tingeso/m3_solicitud_credito/clients/UserFeignClient.java
+++ m3-solicitud-credito/src/main/java/com/tingeso/m3_solicitud_credito/clients/UserFeignClient.java
@@ -1,13 +1,14 @@
 package com.tingeso.m3_solicitud_credito.clients;
 
+import com.tingeso.m3_solicitud_credito.config.FeignClientConfig;
 import com.tingeso.m3_solicitud_credito.model.User;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(value = "m3-solicitud-credito",
+@FeignClient(value = "actual-registro-usuario",
         path = "/register",
-        configuration = {FeignClient.class})
+        configuration = {FeignClientConfig.class})
 public interface UserFeignClient {
     @GetMapping("/{id}")
     User findById(@PathVariable Long id);
